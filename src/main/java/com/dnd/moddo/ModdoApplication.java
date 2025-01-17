2bf6149f984524af45fa6e9b4257320ddfbe28de
sudhdkso <waga1009@gmail.com>
2025-01-17 13:40:54 +0900

    first commit

---
+++ src/main/java/com/dnd/moddo/ModdoApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.moddo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/moddo/ModdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.moddo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
