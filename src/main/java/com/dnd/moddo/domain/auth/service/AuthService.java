d948d1be25f6cad6b2a7abdbe41106b14352d171
조예설 <choyeseol07@gmail.com>
2025-01-31 09:45:03 +0900

    feat: AuthService 추가
    - 비회원 사용자 이메일 임시 발급
    - 비회원 사용자 이름 Guest로 설정

---
+++ src/main/java/com/dnd/moddo/domain/auth/service/AuthService.java
@@ -0,0 +1,40 @@
+package com.dnd.moddo.domain.auth.service;
+
+import com.dnd.moddo.domain.user.entity.User;
+import com.dnd.moddo.domain.user.entity.type.Authority;
+import com.dnd.moddo.domain.user.repository.UserRepository;
+import com.dnd.moddo.global.jwt.dto.TokenResponse;
+import com.dnd.moddo.global.jwt.utill.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    @Transactional
+    public TokenResponse createGuestUser() {
+        String guestEmail = "guest-" + UUID.randomUUID() + "@guest.com";
+
+        User guestUser = User.builder()
+                .email(guestEmail)
+                .name("Guest")
+                .profile(null)
+                .createdAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now().plusMonths(1))
+                .authority(Authority.USER)
+                .isMember(false)
+                .build();
+
+        userRepository.save(guestUser);
+
+        return jwtProvider.generateGuestToken(guestUser.getId(), guestEmail);
+    }
+}
