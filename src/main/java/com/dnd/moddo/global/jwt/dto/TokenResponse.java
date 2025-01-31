95f76af2f258ba972c6f727f26f74fd5961712ce
조예설 <choyeseol07@gmail.com>
2025-01-31 09:40:16 +0900

    feat: JWT 토큰 응답 DTO

---
+++ src/main/java/com/dnd/moddo/global/jwt/dto/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.dnd.moddo.global.jwt.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.ZonedDateTime;
+
+public record TokenResponse(
+        String accessToken,
+        String refreshToken,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        ZonedDateTime expiredAt,
+        Boolean isFirst
+) {
+}
