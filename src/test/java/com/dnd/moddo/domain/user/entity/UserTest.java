0ce274862b2a601ec170d5782f950a9b4d1a3d58
조예설 <choyeseol07@gmail.com>
2025-02-01 23:11:24 +0900

    refactor: 사용하지 않는 import 삭제

--- src/test/java/com/dnd/moddo/domain/user/entity/UserTest.java
+++ src/test/java/com/dnd/moddo/domain/user/entity/UserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
