9a1a0878aa70e9328de480772d386dcd6702b832
조예설 <choyeseol07@gmail.com>
2025-02-01 17:46:49 +0900

    feat: repository 내 findByEmail 예외처리

--- src/main/java/com/dnd/moddo/domain/user/repository/UserRepository.java
+++ src/main/java/com/dnd/moddo/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.dnd.moddo.domain.user.repository;
 
 
 import com.dnd.moddo.domain.user.entity.User;
+import com.dnd.moddo.domain.user.exception.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,17 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
+    default User getByEmail(String email) {
+        return findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(email));
+    }
+
+
+    default User getById(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
 }
