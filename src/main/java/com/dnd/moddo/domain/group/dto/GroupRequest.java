52eda2d079926844ac9fdb5c42dc6bcc9d8cd8d7
조예설 <choyeseol07@gmail.com>
2025-02-04 01:21:07 +0900

    feat: 모임 생성 API

---
+++ src/main/java/com/dnd/moddo/domain/group/dto/GroupRequest.java
@@ -0,0 +1,19 @@
+package com.dnd.moddo.domain.group.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record GroupRequest(
+        @NotBlank(message = "모임 이름은 필수입니다.")
+        String name,
+
+        String password,
+
+        LocalDateTime expiredAt,
+
+        String bank,
+
+        String accountNumber
+) {
+}
--- src/main/java/com/dnd/moddo/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/dnd/moddo/domain/user/exception/UserNotFoundException.java
@@ -4,9 +4,7 @@ import com.dnd.moddo.global.exception.ModdoException;
 import org.springframework.http.HttpStatus;
 
 public class UserNotFoundException extends ModdoException {
-    public UserNotFoundException(String email) {
-        super(HttpStatus.NOT_FOUND, email + "이 이메일인 유저가 없습니다.");
-    }
+    public UserNotFoundException(String email) { super(HttpStatus.NOT_FOUND, email + "이 이메일인 유저가 없습니다."); }
 
     public UserNotFoundException(Long id) {
         super(HttpStatus.NOT_FOUND, id + "인 아이디를 가진 유저가 없습니다.");
