13b9a121ff6a728b1bff963f216d23ab99694717
원지윤 <waga1009@gmail.com>
2025-02-07 08:58:29 +0900

    test: 테스트 코드 공통값 수정

--- src/test/java/com/dnd/moddo/domain/memberExpense/service/implementation/MemberExpenseCreatorTest.java
+++ src/test/java/com/dnd/moddo/domain/memberExpense/service/implementation/MemberExpenseCreatorTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,22 +28,31 @@ class MemberExpenseCreatorTest {
 	@InjectMocks
 	private MemberExpenseCreator memberExpenseCreator;
 
+	private Group mockGroup;
+	private Expense mockExpense;
+	private GroupMember mockGroupMember;
+	private MemberExpenseRequest mockMemberExpenseRequest;
+
+	@BeforeEach
+	void setUp() {
+		mockGroup = new Group("group 1", 1L, "1234", LocalDateTime.now(), LocalDateTime.now().plusMinutes(1),
+			"은행", "계좌");
+		mockExpense = new Expense(mockGroup.getId(), 20000L, "투썸플레이스", LocalDate.of(2025, 02, 03));
+		mockGroupMember = new GroupMember("박완수", mockGroup);
+		mockMemberExpenseRequest = mock(MemberExpenseRequest.class);
+	}
+
 	@DisplayName("지출내역, 참여자 정보가 모두 유효할 때 참여자 지출 내역 생성에 성공한다.")
 	@Test
 	void createMemberExpenseSuccess() {
 		//given
-		Group mockGroup = new Group("group 1", 1L, "1234", LocalDateTime.now(), LocalDateTime.now().plusMinutes(1),
-			"은행", "계좌");
-		Expense expense = new Expense(mockGroup.getId(), 20000L, "투썸플레이스", LocalDate.of(2025, 02, 03));
-		GroupMember mockGroupMember = new GroupMember("박완수", mockGroup);
-		MemberExpenseRequest memberExpenseRequest = mock(MemberExpenseRequest.class);
-
-		MemberExpense mockMemberExpense = new MemberExpense(expense, mockGroupMember, memberExpenseRequest.amount());
-		when(memberExpenseRequest.toEntity(expense, mockGroupMember)).thenReturn(mockMemberExpense);
+		MemberExpense mockMemberExpense = new MemberExpense(mockExpense, mockGroupMember,
+			mockMemberExpenseRequest.amount());
+		when(mockMemberExpenseRequest.toEntity(mockExpense, mockGroupMember)).thenReturn(mockMemberExpense);
 		when(memberExpenseRepository.save(any(MemberExpense.class))).thenReturn(mockMemberExpense);
 
 		//when
-		MemberExpense result = memberExpenseCreator.create(expense, mockGroupMember, memberExpenseRequest);
+		MemberExpense result = memberExpenseCreator.create(mockExpense, mockGroupMember, mockMemberExpenseRequest);
 
 		//then
 		assertThat(result).isEqualTo(mockMemberExpense);
