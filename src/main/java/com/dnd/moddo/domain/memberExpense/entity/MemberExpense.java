9941e8dfc567d02b2be83c18c9595446b0946a3b
원지윤 <waga1009@gmail.com>
2025-02-06 23:40:39 +0900

    feat: MemberExpense엔티티에 @Builder 어노테이션 추가

--- src/main/java/com/dnd/moddo/domain/memberExpense/entity/MemberExpense.java
+++ src/main/java/com/dnd/moddo/domain/memberExpense/entity/MemberExpense.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -33,6 +34,7 @@ public class MemberExpense {
 
 	private Long amount;
 
+	@Builder
 	public MemberExpense(Expense expense, GroupMember groupMember, Long amount) {
 		this.expense = expense;
 		this.groupMember = groupMember;
