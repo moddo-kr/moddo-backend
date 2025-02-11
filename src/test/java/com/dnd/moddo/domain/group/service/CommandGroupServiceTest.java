fe1d5d7df4d4d9ba536fcd6823822061f7d2ecfc
조예설 <choyeseol07@gmail.com>
2025-02-12 08:33:56 +0900

    test: 수정된 코드에 따라 테스트코드 변경

--- src/test/java/com/dnd/moddo/domain/group/service/CommandGroupServiceTest.java
+++ src/test/java/com/dnd/moddo/domain/group/service/CommandGroupServiceTest.java
@@ -5,7 +5,10 @@ import com.dnd.moddo.domain.group.dto.request.GroupRequest;
 import com.dnd.moddo.domain.group.dto.response.GroupResponse;
 import com.dnd.moddo.domain.group.entity.Group;
 import com.dnd.moddo.domain.group.service.implementation.GroupCreator;
+import com.dnd.moddo.domain.group.service.implementation.GroupReader;
 import com.dnd.moddo.domain.group.service.implementation.GroupUpdater;
+import com.dnd.moddo.domain.group.service.implementation.GroupValidator;
+import com.dnd.moddo.global.jwt.dto.GroupTokenResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,6 +34,12 @@ class CommandGroupServiceTest {
     @Mock
     private GroupUpdater groupUpdater; // 추가
 
+    @Mock
+    private GroupReader groupReader;
+
+    @Mock
+    private GroupValidator groupValidator;
+
     @InjectMocks
     private CommandGroupService commandGroupService;
 
@@ -37,12 +47,14 @@ class CommandGroupServiceTest {
     private GroupResponse groupResponse;
     private GroupAccountRequest groupAccountRequest;
     private Group updatedGroup;
+    private GroupTokenResponse expectedResponse;
 
     @BeforeEach
     void setUp() {
         groupRequest = new GroupRequest("GroupName", "password123", LocalDateTime.now());
         groupResponse = new GroupResponse(1L, 1L, LocalDateTime.now(), LocalDateTime.now().minusDays(1), "bank", "1234-1234");
         groupAccountRequest = new GroupAccountRequest("newBank", "5678-5678");
+        expectedResponse = new GroupTokenResponse("access-token", "refresh-token", ZonedDateTime.now());
 
         updatedGroup = mock(Group.class);
     }
@@ -51,28 +63,33 @@ class CommandGroupServiceTest {
     @DisplayName("그룹을 생성할 수 있다.")
     void createGroup() {
         // Given
-        when(groupCreator.createGroup(any(GroupRequest.class), anyLong())).thenReturn(groupResponse);
+        when(groupCreator.createGroup(any(GroupRequest.class), anyLong())).thenReturn(expectedResponse);
 
         // When
-        GroupResponse response = commandGroupService.createGroup(groupRequest, 1L);
+        GroupTokenResponse response = commandGroupService.createGroup(groupRequest, 1L);
 
         // Then
         assertThat(response).isNotNull();
-        assertThat(response.id()).isEqualTo(1L);
-        assertThat(response.writer()).isEqualTo(1L);
+        assertThat(response.accessToken()).isEqualTo("access-token");
+
+        verify(groupCreator, times(1)).createGroup(any(GroupRequest.class), anyLong());
     }
 
     @Test
     @DisplayName("그룹의 계좌 정보를 업데이트할 수 있다.")
     void updateGroupAccount() {
         // Given
+        when(groupReader.read(anyLong())).thenReturn(updatedGroup);
         when(groupUpdater.updateAccount(any(GroupAccountRequest.class), anyLong())).thenReturn(updatedGroup);
+        doNothing().when(groupValidator).checkGroupAuthor(anyLong(), anyLong());
 
         // When
-        GroupResponse result = commandGroupService.updateAccount(groupAccountRequest, 1L);
+        GroupResponse result = commandGroupService.updateAccount(groupAccountRequest, updatedGroup.getWriter(), updatedGroup.getId());
 
         // Then
         assertThat(result).isNotNull();
-        verify(groupUpdater, times(1)).updateAccount(any(GroupAccountRequest.class), anyLong()); // 호출 검증 추가
+        verify(groupReader, times(1)).read(anyLong());
+        verify(groupValidator, times(1)).checkGroupAuthor(anyLong(), anyLong());
+        verify(groupUpdater, times(1)).updateAccount(any(GroupAccountRequest.class), anyLong());
     }
 }
--- src/test/java/com/dnd/moddo/domain/group/service/implementation/GroupCreatorTest.java
+++ src/test/java/com/dnd/moddo/domain/group/service/implementation/GroupCreatorTest.java
@@ -1,11 +1,15 @@
 package com.dnd.moddo.domain.group.service.implementation;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import com.dnd.moddo.domain.user.entity.type.Authority;
+import com.dnd.moddo.global.jwt.dto.GroupTokenResponse;
+import com.dnd.moddo.global.jwt.utill.JwtProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +19,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.dnd.moddo.domain.group.dto.request.GroupRequest;
-import com.dnd.moddo.domain.group.dto.response.GroupResponse;
 import com.dnd.moddo.domain.group.entity.Group;
 import com.dnd.moddo.domain.group.repository.GroupRepository;
 import com.dnd.moddo.domain.user.entity.User;
@@ -25,35 +28,53 @@ import com.dnd.moddo.domain.user.repository.UserRepository;
 class GroupCreatorTest {
     @Mock
     private GroupRepository groupRepository;
+
     @Mock
     private UserRepository userRepository;
+
     @Mock
     private BCryptPasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtProvider jwtProvider;
+
     @InjectMocks
     private GroupCreator groupCreator;
+
+    private Long userId;
+    private GroupRequest request;
+    private User mockUser;
+    private String encodedPassword;
+    private Group mockGroup;
+
+    @BeforeEach
+    void setUp() {
+        userId = 1L;
+        request = new GroupRequest("groupName", "password", LocalDateTime.now().plusDays(1));
+        mockUser = new User(userId, "test@example.com", "닉네임", "프로필", false, LocalDateTime.now(), LocalDateTime.now().plusDays(1), Authority.USER);
+        encodedPassword = "encryptedPassword";
+        mockGroup = new Group(request.name(), userId, "password", LocalDateTime.now(), LocalDateTime.now().plusMonths(1), "groupName", encodedPassword);
+    }
 
     @DisplayName("사용자는 모임 생성에 성공한다.")
     @Test
     void createGroupSuccess() {
         // given
-        Long userId = 1L;
-        GroupRequest request = new GroupRequest("groupName", "password", LocalDateTime.now().plusDays(1));
-        User mockUser = new User(userId, "test@example.com", "닉네임", "프로필", false, LocalDateTime.now(), LocalDateTime.now().plusDays(1), Authority.USER);
-        String encodedPassword = "encryptedPassword";
-
         when(userRepository.getById(userId)).thenReturn(mockUser);
         when(passwordEncoder.encode(request.password())).thenReturn(encodedPassword);
-        when(groupRepository.save(any(Group.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(groupRepository.save(any(Group.class))).thenReturn(mockGroup);
+        when(jwtProvider.generateGroupToken(mockGroup.getWriter(), mockGroup.getId())).thenReturn(new GroupTokenResponse("access-token", "refresh-token", ZonedDateTime.now()));
 
         // when
-        GroupResponse response = groupCreator.createGroup(request, userId);
+        GroupTokenResponse response = groupCreator.createGroup(request, userId);
 
         // then
         assertThat(response).isNotNull();
-        assertThat(response.writer()).isEqualTo(userId);
+        assertThat(response.accessToken()).isEqualTo("access-token");
 
         verify(userRepository, times(1)).getById(userId);
         verify(passwordEncoder, times(1)).encode(request.password());
         verify(groupRepository, times(1)).save(any(Group.class));
+        verify(jwtProvider, times(1)).generateGroupToken(mockGroup.getWriter(), mockGroup.getId());
     }
 }
