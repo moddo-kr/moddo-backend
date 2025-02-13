783e1b34beff9ec9bdfcbd55710fd7a5fc2e3bdb
조예설 <choyeseol07@gmail.com>
2025-02-13 18:23:01 +0900

    chore: 모임 Id 토큰으로 변경

--- src/main/java/com/dnd/moddo/domain/groupMember/controller/GroupMemberController.java
+++ src/main/java/com/dnd/moddo/domain/groupMember/controller/GroupMemberController.java
@@ -1,5 +1,6 @@
 package com.dnd.moddo.domain.groupMember.controller;
 
+import com.dnd.moddo.global.jwt.service.JwtService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,36 +28,39 @@ import lombok.RequiredArgsConstructor;
 public class GroupMemberController {
 	private final QueryGroupMemberService queryGroupMemberService;
 	private final CommandGroupMemberService commandGroupMemberService;
+	private final JwtService jwtService;
 
 	@PostMapping
 	public ResponseEntity<GroupMembersResponse> saveGroupMembers(
-		@RequestParam("groupId") Long groupId,
-		@Valid @RequestBody GroupMembersSaveRequest request
+			@RequestParam("groupToken") String groupToken,
+			@Valid @RequestBody GroupMembersSaveRequest request
 	) {
+		Long groupId = jwtService.getGroupId(groupToken);
 		GroupMembersResponse response = commandGroupMemberService.create(groupId, request);
 		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping
 	public ResponseEntity<GroupMembersResponse> getGroupMembers(
-		@RequestParam("groupId") Long groupId
+			@RequestParam("groupToken") String groupToken
 	) {
+		Long groupId = jwtService.getGroupId(groupToken);
 		GroupMembersResponse response = queryGroupMemberService.findAll(groupId);
 		return ResponseEntity.ok(response);
 	}
 
 	@PutMapping
 	public ResponseEntity<GroupMemberResponse> addGroupMember(
-		@RequestParam("groupId") Long groupId,
-		@Valid @RequestBody GroupMemberSaveRequest request
+			@RequestParam("groupToken") String groupToken,
+			@Valid @RequestBody GroupMemberSaveRequest request
 	) {
+		Long groupId = jwtService.getGroupId(groupToken);
 		GroupMemberResponse response = commandGroupMemberService.addGroupMember(groupId, request);
 		return ResponseEntity.ok(response);
 	}
 
 	@DeleteMapping("/{groupMemberId}")
 	public ResponseEntity<Void> deleteGroupMember(
-		@RequestParam("groupId") Long groupId,
 		@PathVariable("groupMemberId") Long groupMemberId
 	) {
 		commandGroupMemberService.delete(groupMemberId);
