10ea269041669c95f1d2bb48cbdcb5b24b5b1753
조예설 <choyeseol07@gmail.com>
2025-02-14 16:51:57 +0900

    chore: Id를 groupId로 변경

--- src/main/java/com/dnd/moddo/domain/group/repository/GroupRepository.java
+++ src/main/java/com/dnd/moddo/domain/group/repository/GroupRepository.java
@@ -4,8 +4,10 @@ import com.dnd.moddo.domain.group.entity.Group;
 import com.dnd.moddo.domain.group.exception.GroupNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    default Group getById(Long id) {
-        return findById(id).orElseThrow(() -> new GroupNotFoundException(id));
+    default Group getById(Long groupId) {
+        return findById(groupId).orElseThrow(() -> new GroupNotFoundException(groupId));
     }
 }
