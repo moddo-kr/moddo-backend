806098e7261b5a81a3bd1af07b62a95125b824c4
조예설 <choyeseol07@gmail.com>
2025-02-14 23:45:20 +0900

    chore: 불필요한 임포트 제거

--- src/main/java/com/dnd/moddo/domain/group/repository/GroupRepository.java
+++ src/main/java/com/dnd/moddo/domain/group/repository/GroupRepository.java
@@ -4,8 +4,6 @@ import com.dnd.moddo.domain.group.entity.Group;
 import com.dnd.moddo.domain.group.exception.GroupNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface GroupRepository extends JpaRepository<Group, Long> {
     default Group getById(Long groupId) {
         return findById(groupId).orElseThrow(() -> new GroupNotFoundException(groupId));
