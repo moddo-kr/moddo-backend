5881eb7270a1c3f22fc92a93d55e012222fd21a0
조예설 <choyeseol07@gmail.com>
2025-02-15 11:30:07 +0900

    chore: getS3Url 추가

--- src/main/java/com/dnd/moddo/global/config/S3Bucket.java
+++ src/main/java/com/dnd/moddo/global/config/S3Bucket.java
@@ -9,4 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class S3Bucket {
     @Value("${aws.s3.bucket}")
     private String s3Bucket;
+
+    public String getS3Url() {
+        return "https://" + s3Bucket + ".s3.amazonaws.com/";
+    }
 }
