f07dbc694361f2c7cb55d71833efca5a966df54e
조예설 <choyeseol07@gmail.com>
2025-02-15 11:35:23 +0900

    test: ImageTest

---
+++ src/test/java/com/dnd/moddo/domain/image/entity/ImageTest.java
@@ -0,0 +1,28 @@
+package com.dnd.moddo.domain.image.entity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+
+    private Image image;
+
+    @BeforeEach
+    void setUp() {
+        // given
+        image = new Image("uniqueKey", "/images/path/to/image.jpg");
+    }
+
+    @Test
+    void createImage() {
+        // when
+        String uniqueKey = image.getUniqueKey();
+        String path = image.getPath();
+
+        // then
+        assertThat(uniqueKey).isEqualTo("uniqueKey");
+        assertThat(path).isEqualTo("/images/path/to/image.jpg");
+    }
+}
