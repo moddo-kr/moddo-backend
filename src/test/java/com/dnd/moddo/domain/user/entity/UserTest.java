c37722ecfdcbfbe0f0390e91fd4670522afd117c
조예설 <choyeseol07@gmail.com>
2025-02-16 20:20:59 +0900

    test: 테스트 코드 에러 수정

--- src/test/java/com/dnd/moddo/domain/user/entity/UserTest.java
+++ src/test/java/com/dnd/moddo/domain/user/entity/UserTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -18,8 +20,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ModdoApplication.class)
-@SpringBootTest
+@DataJpaTest
 public class UserTest {
 
     @Autowired
