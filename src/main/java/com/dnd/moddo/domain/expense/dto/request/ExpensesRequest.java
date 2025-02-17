6ce86810f4e87872d8eaac92ba7ee3ac3bb26ae2
원지윤 <waga1009@gmail.com>
2025-02-18 01:14:26 +0900

    feat: 입력값에 대한 validation추가

--- src/main/java/com/dnd/moddo/domain/expense/dto/request/ExpensesRequest.java
+++ src/main/java/com/dnd/moddo/domain/expense/dto/request/ExpensesRequest.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.dnd.moddo.domain.expense.entity.Expense;
 import com.dnd.moddo.domain.group.entity.Group;
 
+import jakarta.validation.Valid;
+
 public record ExpensesRequest(
-	List<ExpenseRequest> expenses
+	@Valid List<ExpenseRequest> expenses
 ) {
 	public List<Expense> toEntity(Group group) {
 		return expenses.stream()
--- src/main/java/com/dnd/moddo/domain/groupMember/dto/request/GroupMemberSaveRequest.java
+++ src/main/java/com/dnd/moddo/domain/groupMember/dto/request/GroupMemberSaveRequest.java
@@ -5,9 +5,11 @@ import com.dnd.moddo.domain.groupMember.entity.GroupMember;
 import com.dnd.moddo.domain.groupMember.entity.type.ExpenseRole;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record GroupMemberSaveRequest(
-	@NotBlank(message = "참여자 이름으로 공백은 입력할 수 없습니다.")
+	@NotBlank(message = "이름은 필수 입력값 입니다.")
+	@Pattern(regexp = "^[가-힣a-zA-Z]{1,5}$", message = "참여자 이름은 한글과 영어만 포함하여 5자 이내여야 합니다.")
 	String name
 
 ) {
