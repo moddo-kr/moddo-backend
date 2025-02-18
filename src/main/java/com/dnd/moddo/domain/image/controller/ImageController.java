98d95023d3cd32e6a7bad9e952a14e93cbd64cc8
조예설 <choyeseol07@gmail.com>
2025-02-19 02:08:42 +0900

    feat: QueryService, Controller

--- src/main/java/com/dnd/moddo/domain/image/controller/ImageController.java
+++ src/main/java/com/dnd/moddo/domain/image/controller/ImageController.java
@@ -1,8 +1,10 @@
 package com.dnd.moddo.domain.image.controller;
 
+import com.dnd.moddo.domain.image.dto.CharacterResponse;
 import com.dnd.moddo.domain.image.dto.ImageResponse;
 import com.dnd.moddo.domain.image.dto.TempImageResponse;
 import com.dnd.moddo.domain.image.service.CommandImageService;
+import com.dnd.moddo.domain.image.service.QueryImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ import java.util.List;
 public class ImageController {
     private final CommandImageService commandImageService;
 
+    private final QueryImageService queryImageService;
+
     @PostMapping("/temp")
     public ResponseEntity<TempImageResponse> saveTempImage(@RequestParam("file") List<MultipartFile> files) {
         TempImageResponse uniqueKey = commandImageService.uploadTempImage(files);
@@ -26,6 +30,12 @@ public class ImageController {
     public ResponseEntity<ImageResponse> updateImage(@RequestParam("uniqueKey") List<String> uniqueKeys) {
         ImageResponse finalImagePath = commandImageService.uploadFinalImage(uniqueKeys);
         return ResponseEntity.ok(finalImagePath);
+    }
+
+    @GetMapping("/character")
+    public ResponseEntity<CharacterResponse> getRandomCharacter() {
+        CharacterResponse character = queryImageService.getCharacter();
+        return ResponseEntity.ok(character);
     }
 }
 
