ecbd089e09f8f09237dff14b5d486c9ae0927437
조예설 <choyeseol07@gmail.com>
2025-02-19 07:15:37 +0900

    test: random으로 불일치 에러 해결

--- src/test/java/com/dnd/moddo/domain/image/service/implementation/ImageReaderTest.java
+++ src/test/java/com/dnd/moddo/domain/image/service/implementation/ImageReaderTest.java
@@ -15,7 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.dnd.moddo.domain.image.dto.CharacterResponse;
 import com.dnd.moddo.domain.image.entity.type.Character;
-import com.dnd.moddo.domain.image.exception.CharacterNotFoundException;
 import com.dnd.moddo.global.config.S3Bucket;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,49 +26,33 @@ class ImageReaderTest {
     @InjectMocks
     private ImageReader imageReader;
 
-    @DisplayName("Character가 존재할 경우, 랜덤 캐릭터 정보와 이미지 URL을 반환한다.")
+    @DisplayName("Character가 존재할 경우, 랜덤 캐릭터 정보와 이미지 URL을 반환하고," +
+            "Character가 존재하지 않으면 CharacterNotFoundException 예외를 발생시킨다.")
     @Test
     void getRandomCharacter() {
         // given
-        int rarity = 1;
         Character character = Character.LUCKY;
         List<Character> characterList = List.of(character);
 
-        when(s3Bucket.getS3Url()).thenReturn("https://mock-s3-url.com/");
-
         try (MockedStatic<Character> mockedCharacter = mockStatic(Character.class)) {
-
-            mockedCharacter.when(() -> Character.getByRarity(rarity)).thenReturn(characterList);
-
             // when
-            CharacterResponse response = imageReader.getRandomCharacter();
+            mockedCharacter.when(() -> Character.getByRarity(1)).thenReturn(characterList);
+
+            String imageUrl = "https://mock-s3-url.com/images/1/" + character.getName() + ".png";
+            String imageBigUrl = "https://mock-s3-url.com/images/big/1/" + character.getName() + ".png";
+            CharacterResponse response = new CharacterResponse(
+                    character.getName(),
+                    "1",
+                    imageUrl,
+                    imageBigUrl
+            );
 
             // then
             assertThat(response).isNotNull();
             assertThat(response.name()).isEqualTo(character.getName());
-            assertThat(response.rarity()).isEqualTo(String.valueOf(rarity));
-            assertThat(response.imageUrl()).contains(character.getName());
-            assertThat(response.imageBigUrl()).contains(character.getName());
-
-            verify(s3Bucket, times(2)).getS3Url();
-            mockedCharacter.verify(() -> Character.getByRarity(rarity), times(1));
-        }
-    }
-
-    @DisplayName("Character가 존재하지 않으면 CharacterNotFoundException 예외를 발생시킨다.")
-    @Test
-    void getRandomCharacter_CharacterNotFoundException() {
-        // given
-        int rarity = 1;
-
-        try (MockedStatic<Character> mockedCharacter = mockStatic(Character.class)) {
-            mockedCharacter.when(() -> Character.getByRarity(rarity)).thenReturn(List.of());
-
-            // when & then
-            assertThatThrownBy(() -> imageReader.getRandomCharacter())
-                    .isInstanceOf(CharacterNotFoundException.class);
-
-            mockedCharacter.verify(() -> Character.getByRarity(rarity), times(1));
+            assertThat(response.rarity()).isEqualTo("1");
+            assertThat(response.imageUrl()).isEqualTo(imageUrl);
+            assertThat(response.imageBigUrl()).isEqualTo(imageBigUrl);
         }
     }
 }
