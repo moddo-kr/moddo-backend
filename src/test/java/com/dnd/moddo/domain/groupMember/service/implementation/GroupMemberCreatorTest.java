5b10fe28f97d4f577d3b3c22fe6979273cbddba7
조예설 <choyeseol07@gmail.com>
2025-02-21 17:14:14 +0900

    test: GroupMemberCreatorTest 오류 수정

--- src/test/java/com/dnd/moddo/domain/groupMember/service/implementation/GroupMemberCreatorTest.java
+++ src/test/java/com/dnd/moddo/domain/groupMember/service/implementation/GroupMemberCreatorTest.java
@@ -38,7 +38,7 @@ public class GroupMemberCreatorTest {
 		mockGroup = mock(Group.class);
 	}
 
-	@DisplayName("사용자가 비회원인 경우, 총무의 이름은 '김모또'로 생성되고 프로필 URL이 동적으로 생성된다.")
+	@DisplayName("사용자가 비회원인 경우, 총무의 이름은 '김모또'로 생성되고 프로필 ID가 0으로 설정된다.")
 	@Test
 	void create_Success_WithGuestMember() {
 		// given
@@ -51,7 +51,7 @@ public class GroupMemberCreatorTest {
 		GroupMember expectedMember = GroupMember.builder()
 			.name("김모또")
 			.group(mockGroup)
-			.profileId(null)
+			.profileId(0)
 			.role(ExpenseRole.MANAGER)
 			.build();
 
@@ -64,13 +64,13 @@ public class GroupMemberCreatorTest {
 		assertThat(savedMember).isNotNull();
 		assertThat(savedMember.getName()).isEqualTo("김모또");
 		assertThat(savedMember.getRole()).isEqualTo(ExpenseRole.MANAGER);
-		assertThat(savedMember.getProfileUrl()).isEqualTo("https://moddo-s3.s3.amazonaws.com/profile/MODDO.png");
+		assertThat(savedMember.getProfileId()).isEqualTo(0); // profileId 검증
 
 		verify(userRepository, times(1)).getById(eq(userId));
 		verify(groupMemberRepository, times(1)).save(any(GroupMember.class));
 	}
 
-	@DisplayName("사용자가 회원인 경우, 총무의 이름은 회원의 이름으로 생성되고 프로필 URL이 동적으로 생성된다.")
+	@DisplayName("사용자가 회원인 경우, 총무의 이름은 회원의 이름으로 생성되고 프로필 ID가 0으로 설정된다.")
 	@Test
 	void create_Success_WithMember() {
 		// given
@@ -97,7 +97,7 @@ public class GroupMemberCreatorTest {
 		assertThat(savedMember).isNotNull();
 		assertThat(savedMember.getName()).isEqualTo("연노른자");
 		assertThat(savedMember.getRole()).isEqualTo(ExpenseRole.MANAGER);
-		assertThat(savedMember.getProfileUrl()).isEqualTo("https://moddo-s3.s3.amazonaws.com/profile/MODDO.png");
+		assertThat(savedMember.getProfileId()).isEqualTo(0);
 
 		verify(userRepository, times(1)).getById(eq(userId));
 		verify(groupMemberRepository, times(1)).save(any(GroupMember.class));
