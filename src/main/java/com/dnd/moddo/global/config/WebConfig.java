ae96ec08764718052e3a1ff3d62fff8aa73b929e
원지윤 <waga1009@gmail.com>
2025-04-17 23:48:51 +0900

    feat: CORS 허용 origin 목록에 localhost:4173 추가

--- src/main/java/com/dnd/moddo/global/config/WebConfig.java
+++ src/main/java/com/dnd/moddo/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("https://www.moddo.kr", "http://localhost:3000")
+			.allowedOrigins("https://www.moddo.kr", "http://localhost:3000", "http://localhost:4173")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
