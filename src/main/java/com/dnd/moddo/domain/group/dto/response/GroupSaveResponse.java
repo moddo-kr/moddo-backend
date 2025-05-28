f96f8136c623f96250af6159b2bb5fb865323cf4
원지윤 <waga1009@gmail.com>
2025-05-29 00:58:43 +0900

    feat: group 생성시 토큰 -> 코드를 response 하도록 변경

--- src/main/java/com/dnd/moddo/domain/group/dto/response/GroupSaveResponse.java
+++ src/main/java/com/dnd/moddo/domain/group/dto/response/GroupSaveResponse.java
@@ -3,7 +3,7 @@ package com.dnd.moddo.domain.group.dto.response;
 import com.dnd.moddo.domain.groupMember.dto.response.GroupMemberResponse;
 
 public record GroupSaveResponse(
-	String groupToken,
+	String code,
 	GroupMemberResponse manager
 ) {
 }
