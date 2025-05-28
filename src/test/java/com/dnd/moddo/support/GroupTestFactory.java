355577b81f601b77bd26e3667eed3dbebdf4b269
원지윤 <waga1009@gmail.com>
2025-05-28 15:53:21 +0900

    test: 그룹 테스트용 GroupTestFactory 클래스 생성

---
+++ src/test/java/com/dnd/moddo/support/GroupTestFactory.java
@@ -0,0 +1,33 @@
+package com.dnd.moddo.support;
+
+import java.time.LocalDateTime;
+
+import com.dnd.moddo.domain.group.entity.Group;
+
+public class GroupTestFactory {
+	public static Group createDefault() {
+		return new Group(
+			"group 1",
+			1L,
+			"1234",
+			LocalDateTime.now().plusMinutes(1),
+			"은행",
+			"계좌",
+			"A1B2C3D4",
+			LocalDateTime.now().plusDays(1)
+		);
+	}
+
+	public static Group createWithCode(String code) {
+		return new Group(
+			"group 1",
+			1L,
+			"1234",
+			LocalDateTime.now().plusMinutes(1),
+			"은행",
+			"계좌",
+			code,
+			LocalDateTime.now().plusDays(1)
+		);
+	}
+}
