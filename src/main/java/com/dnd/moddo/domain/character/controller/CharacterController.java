e91022268d4aca72f0eb839dd10b9dfcdc1785b7
원지윤 <waga1009@gmail.com>
2025-05-29 01:01:40 +0900

    feat: token기반 조회에서 code기반 조회로 변경

--- src/main/java/com/dnd/moddo/domain/character/controller/CharacterController.java
+++ src/main/java/com/dnd/moddo/domain/character/controller/CharacterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dnd.moddo.domain.character.service.QueryCharacterService;
+import com.dnd.moddo.domain.group.service.QueryGroupService;
 import com.dnd.moddo.domain.image.dto.CharacterResponse;
 import com.dnd.moddo.global.jwt.service.JwtService;
 
@@ -19,12 +20,13 @@ public class CharacterController {
 
 	private final JwtService jwtService;
 	private final QueryCharacterService queryCharacterService;
+	private final QueryGroupService queryGroupService;
 
 	@GetMapping()
 	public ResponseEntity<CharacterResponse> getCharacter(
-		@RequestParam("groupToken") String groupToken
+		@RequestParam("code") String code
 	) {
-		Long groupId = jwtService.getGroupId(groupToken);
+		Long groupId = queryGroupService.findIdByCode(code);
 
 		CharacterResponse response = queryCharacterService.findCharacterByGroupId(groupId);
 		return ResponseEntity.ok(response);
--- src/main/java/com/dnd/moddo/domain/memberExpense/controller/MemberExpenseController.java
+++ src/main/java/com/dnd/moddo/domain/memberExpense/controller/MemberExpenseController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dnd.moddo.domain.group.service.QueryGroupService;
 import com.dnd.moddo.domain.groupMember.dto.response.GroupMembersExpenseResponse;
 import com.dnd.moddo.domain.memberExpense.service.QueryMemberExpenseService;
 import com.dnd.moddo.global.jwt.service.JwtService;
@@ -18,12 +19,13 @@ import lombok.RequiredArgsConstructor;
 public class MemberExpenseController {
 	private final QueryMemberExpenseService queryMemberExpenseService;
 	private final JwtService jwtService;
+	private final QueryGroupService queryGroupService;
 
 	@GetMapping
 	public ResponseEntity<GroupMembersExpenseResponse> getMemberExpensesDetails(
-		@RequestParam("groupToken") String groupToken
+		@RequestParam("code") String code
 	) {
-		Long groupId = jwtService.getGroupId(groupToken);
+		Long groupId = queryGroupService.findIdByCode(code);
 		GroupMembersExpenseResponse response = queryMemberExpenseService.findMemberExpenseDetailsByGroupId(groupId);
 		return ResponseEntity.ok(response);
 	}
