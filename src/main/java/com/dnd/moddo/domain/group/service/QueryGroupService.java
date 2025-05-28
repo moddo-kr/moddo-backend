05f1ecb11cb9727cdd16b5ec4cd183a56a9848b5
원지윤 <waga1009@gmail.com>
2025-05-28 15:15:50 +0900

    feat: 그룹코드를 통해 그룹ID를 조회하는 기능 추가

--- src/main/java/com/dnd/moddo/domain/group/service/QueryGroupService.java
+++ src/main/java/com/dnd/moddo/domain/group/service/QueryGroupService.java
@@ -1,30 +1,36 @@
 package com.dnd.moddo.domain.group.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.dnd.moddo.domain.group.dto.response.GroupDetailResponse;
 import com.dnd.moddo.domain.group.dto.response.GroupHeaderResponse;
 import com.dnd.moddo.domain.group.entity.Group;
 import com.dnd.moddo.domain.group.service.implementation.GroupReader;
 import com.dnd.moddo.domain.group.service.implementation.GroupValidator;
 import com.dnd.moddo.domain.groupMember.entity.GroupMember;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class QueryGroupService {
-    private final GroupReader groupReader;
-    private final GroupValidator groupValidator;
+	private final GroupReader groupReader;
+	private final GroupValidator groupValidator;
 
-    public GroupDetailResponse findOne(Long groupId, Long userId) {
-        Group group = groupReader.read(groupId);
-        groupValidator.checkGroupAuthor(group, userId);
-        List<GroupMember> members = groupReader.findByGroup(groupId);
-        return GroupDetailResponse.of(group, members);
-    }
+	public GroupDetailResponse findOne(Long groupId, Long userId) {
+		Group group = groupReader.read(groupId);
+		groupValidator.checkGroupAuthor(group, userId);
+		List<GroupMember> members = groupReader.findByGroup(groupId);
+		return GroupDetailResponse.of(group, members);
+	}
 
-    public GroupHeaderResponse findByGroupHeader(Long groupId) {
-        return groupReader.findByHeader(groupId);
-    }
+	public GroupHeaderResponse findByGroupHeader(Long groupId) {
+		return groupReader.findByHeader(groupId);
+	}
+
+	public Long findIdByCode(String code) {
+		return groupReader.findIdByGroupCode(code);
+	}
 }
