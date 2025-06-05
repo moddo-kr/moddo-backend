b36b47a81b46b995ded8f75dfc5a4d37dc7e7e25
조예설 <choyeseol07@gmail.com>
2025-06-05 11:59:01 +0900

    Docs: 문서 형식 일정하게 변경

--- src/test/java/com/dnd/moddo/domain/expense/controller/ExpenseControllerTest.java
+++ src/test/java/com/dnd/moddo/domain/expense/controller/ExpenseControllerTest.java
@@ -1,11 +1,13 @@
 package com.dnd.moddo.domain.expense.controller;
 
-import static org.mockito.Mockito.*;
+import static com.dnd.moddo.domain.groupMember.entity.type.ExpenseRole.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,33 +17,55 @@ import org.springframework.http.MediaType;
 import com.dnd.moddo.domain.expense.dto.request.ExpenseImageRequest;
 import com.dnd.moddo.domain.expense.dto.request.ExpenseRequest;
 import com.dnd.moddo.domain.expense.dto.request.ExpensesRequest;
+import com.dnd.moddo.domain.expense.dto.response.ExpenseDetailResponse;
 import com.dnd.moddo.domain.expense.dto.response.ExpenseDetailsResponse;
 import com.dnd.moddo.domain.expense.dto.response.ExpenseResponse;
 import com.dnd.moddo.domain.expense.dto.response.ExpensesResponse;
 import com.dnd.moddo.domain.expense.exception.ExpenseNotFoundException;
 import com.dnd.moddo.domain.groupMember.exception.GroupMemberNotFoundException;
+import com.dnd.moddo.domain.memberExpense.dto.request.MemberExpenseRequest;
+import com.dnd.moddo.domain.memberExpense.dto.response.MemberExpenseResponse;
 import com.dnd.moddo.global.util.RestDocsTestSupport;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ExpenseControllerTest extends RestDocsTestSupport {
 
 	private final String groupToken = "groupToken";
 	private final Long groupId = 1L;
-	private final Long expenseId = 100L;
-
-	private final ObjectMapper objectMapper = new ObjectMapper()
-		.registerModule(new JavaTimeModule())
-		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	private final Long expenseId = 1L;
 
 	@Test
-	@DisplayName("지출을 정상적으로 저장한다")
+	@DisplayName("지출 내역을 정상적으로 저장한다.")
 	void saveExpensesSuccess() throws Exception {
-		ExpensesRequest request = new ExpensesRequest(
-			List.of(new ExpenseRequest(100000L, "지출", LocalDate.now(), List.of()))
-		);
-		ExpensesResponse response = new ExpensesResponse(Collections.emptyList());
+		ExpensesRequest request = new ExpensesRequest(List.of(
+			new ExpenseRequest(
+				20000L, "카페", LocalDate.of(2025, 2, 3),
+				List.of(
+					new MemberExpenseRequest(1L, 9000L),
+					new MemberExpenseRequest(2L, 11000L)
+				)
+			)
+		));
+
+		ExpensesResponse response = new ExpensesResponse(List.of(
+			new ExpenseResponse(
+				1L, 100000L, "하이디라오", LocalDate.of(2025, 2, 3),
+				List.of(
+					new MemberExpenseResponse(1L, MANAGER, "김모또",
+						"https://moddo-s3.s3.amazonaws.com/profile/MODDO.png", 50000L),
+					new MemberExpenseResponse(2L, PARTICIPANT, "군계란",
+						"https://moddo-s3.s3.amazonaws.com/profile/1.png", 50000L)
+				)
+			),
+			new ExpenseResponse(
+				2L, 20000L, "카페", LocalDate.of(2025, 2, 3),
+				List.of(
+					new MemberExpenseResponse(1L, MANAGER, "김모또",
+						"https://moddo-s3.s3.amazonaws.com/profile/MODDO.png", 9000L),
+					new MemberExpenseResponse(2L, PARTICIPANT, "군계란",
+						"https://moddo-s3.s3.amazonaws.com/profile/1.png", 11000L)
+				)
+			)
+		));
 
 		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
 		when(commandExpenseService.createExpenses(eq(groupId), any())).thenReturn(response);
@@ -54,80 +78,162 @@ public class ExpenseControllerTest extends RestDocsTestSupport {
 	}
 
 	@Test
-	@DisplayName("전체 지출 목록을 정상적으로 조회한다")
+	@DisplayName("전체 지출 목록을 정상적으로 조회한다.")
 	void getAllByGroupIdSuccess() throws Exception {
-		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
-		when(queryExpenseService.findAllByGroupId(groupId)).thenReturn(new ExpensesResponse(List.of()));
+		// given
+		List<ExpenseResponse> expenseResponses = List.of(
+			new ExpenseResponse(1L, 100000L, "지출", LocalDate.of(2025, 2, 3), List.of(
+				new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+					1000L),
+				new MemberExpenseResponse(2L, PARTICIPANT, "군계란", "https://moddo-s3.s3.amazonaws.com/profile/1.png",
+					50000L)
+			)),
+			new ExpenseResponse(2L, 22000L, "카페", LocalDate.of(2025, 2, 3), List.of(
+				new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+					10000L),
+				new MemberExpenseResponse(2L, PARTICIPANT, "군계란", "https://moddo-s3.s3.amazonaws.com/profile/1.png",
+					12000L)
+			)),
+			new ExpenseResponse(3L, 210000L, "향수공방", LocalDate.of(2025, 2, 3), List.of(
+				new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+					70000L),
+				new MemberExpenseResponse(2L, PARTICIPANT, "군계란", "https://moddo-s3.s3.amazonaws.com/profile/1.png",
+					70000L),
+				new MemberExpenseResponse(3L, PARTICIPANT, "연노른자", "https://moddo-s3.s3.amazonaws.com/profile/2.png",
+					70000L)
+			)),
+			new ExpenseResponse(4L, 36000L, "간술", LocalDate.of(2025, 2, 3), List.of(
+				new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+					12000L),
+				new MemberExpenseResponse(2L, PARTICIPANT, "군계란", "https://moddo-s3.s3.amazonaws.com/profile/1.png",
+					12000L),
+				new MemberExpenseResponse(3L, PARTICIPANT, "연노른자", "https://moddo-s3.s3.amazonaws.com/profile/2.png",
+					12000L)
+			))
+		);
 
+		ExpensesResponse response = new ExpensesResponse(expenseResponses);
+
+		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
+		when(queryExpenseService.findAllByGroupId(groupId)).thenReturn(response);
+
+		// when & then
 		mockMvc.perform(get("/api/v1/expenses")
 				.param("groupToken", groupToken))
-			.andExpect(status().isOk());
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.expenses.length()").value(4))
+			.andExpect(jsonPath("$.expenses[0].id").value(1))
+			.andExpect(jsonPath("$.expenses[0].memberExpenses[0].name").value("김모또"));
 	}
 
 	@Test
-	@DisplayName("단일 지출 내역을 정상적으로 조회한다")
+	@DisplayName("단일 지출 내역을 정상적으로 조회한다.")
 	void getByExpenseIdSuccess() throws Exception {
-		when(queryExpenseService.findOneByExpenseId(expenseId)).thenReturn(mock(ExpenseResponse.class));
+		// given
+		ExpenseResponse response = new ExpenseResponse(
+			1L, 25000L, "카페", LocalDate.of(2025, 2, 3), List.of(
+			new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+				12000L))
+		);
 
-		mockMvc.perform(get("/api/v1/expenses/{expenseId}", expenseId))
-			.andExpect(status().isOk());
-	}
+		when(queryExpenseService.findOneByExpenseId(expenseId)).thenReturn(response);
 
-	@Test
-	@DisplayName("지출 상세 내역을 정상적으로 조회한다")
-	void getExpenseDetailsSuccess() throws Exception {
-		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
-		when(queryExpenseService.findAllExpenseDetailsByGroupId(groupId)).thenReturn(
-			mock(ExpenseDetailsResponse.class));
-
-		mockMvc.perform(get("/api/v1/expenses/details")
+		// when & then
+		mockMvc.perform(get("/api/v1/expenses/{expenseId}", expenseId)
 				.param("groupToken", groupToken))
 			.andExpect(status().isOk());
 	}
 
 	@Test
-	@DisplayName("지출을 정상적으로 수정한다")
+	@DisplayName("지출 상세 내역을 정상적으로 조회한다.")
+	void getExpenseDetailsSuccess() throws Exception {
+		// given
+		List<ExpenseDetailResponse> expenseDetail = List.of(
+			new ExpenseDetailResponse(1L, LocalDate.of(2025, 2, 3), "하이디라오", 100000L, List.of("김모또(총무)", "김반숙")),
+			new ExpenseDetailResponse(2L, LocalDate.of(2025, 2, 3), "카페", 22000L, List.of("김모또(총무)", "김반숙")),
+			new ExpenseDetailResponse(3L, LocalDate.of(2025, 2, 3), "향수공방", 210000L, List.of("김모또(총무)", "김반숙", "정에그")),
+			new ExpenseDetailResponse(4L, LocalDate.of(2025, 2, 3), "간술", 36000L, List.of("김모또(총무)", "김반숙", "정에그"))
+		);
+		ExpenseDetailsResponse response = new ExpenseDetailsResponse(expenseDetail);
+
+		given(jwtService.getGroupId(groupToken)).willReturn(groupId);
+		given(queryExpenseService.findAllExpenseDetailsByGroupId(groupId)).willReturn(response);
+
+		// when & then
+		mockMvc.perform(get("/api/v1/expenses/details")
+				.param("groupToken", groupToken))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.expenses", hasSize(4)))
+			.andExpect(jsonPath("$.expenses[0].id").value(1))
+			.andExpect(jsonPath("$.expenses[0].content").value("하이디라오"))
+			.andExpect(jsonPath("$.expenses[0].totalAmount").value(100000))
+			.andExpect(jsonPath("$.expenses[0].groupMembers[0]").value("김모또(총무)"));
+	}
+
+	@Test
+	@DisplayName("지출 내역을 정상적으로 수정한다.")
 	void updateExpenseSuccess() throws Exception {
-		ExpenseRequest request = new ExpenseRequest(100000L, "지출", LocalDate.now(), List.of());
+		// given
+		ExpenseRequest request = new ExpenseRequest(
+			28000L,
+			"카페",
+			LocalDate.of(2025, 2, 2),
+			List.of(
+				new MemberExpenseRequest(1L, 14000L),
+				new MemberExpenseRequest(2L, 14000L)
+			)
+		);
 
-		when(commandExpenseService.update(eq(expenseId), any())).thenReturn(mock(ExpenseResponse.class));
+		ExpenseResponse response = new ExpenseResponse(
+			1L,
+			28000L,
+			"카페",
+			LocalDate.of(2025, 2, 2),
+			List.of(
+				new MemberExpenseResponse(1L, MANAGER, "김모또", "https://moddo-s3.s3.amazonaws.com/profile/MODDO.png",
+					14000L),
+				new MemberExpenseResponse(2L, PARTICIPANT, "김반숙", "https://moddo-s3.s3.amazonaws.com/profile/1.png",
+					14000L)
+			)
+		);
 
+		given(commandExpenseService.update(eq(expenseId), any())).willReturn(response);
+
+		// when & then
 		mockMvc.perform(put("/api/v1/expenses/{expenseId}", expenseId)
+				.param("groupToken", groupToken)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(request)))
 			.andExpect(status().isOk());
 	}
 
 	@Test
-	@DisplayName("지출을 정상적으로 삭제한다")
+	@DisplayName("지출 내역을 정상적으로 삭제한다.")
 	void deleteExpenseSuccess() throws Exception {
 		doNothing().when(commandExpenseService).delete(expenseId);
 
-		mockMvc.perform(delete("/api/v1/expenses/{expenseId}", expenseId))
+		mockMvc.perform(delete("/api/v1/expenses/{expenseId}", expenseId)
+				.param("groupToken", groupToken))
 			.andExpect(status().isNoContent());
 	}
 
 	@Test
-	@DisplayName("지출 이미지 URL을 정상적으로 업데이트한다")
+	@DisplayName("지출 이미지 URL을 정상적으로 업데이트한다.")
 	void updateImgUrlSuccess() throws Exception {
 		ExpenseImageRequest request = new ExpenseImageRequest(List.of(
 			"https://image1.com",
 			"https://image2.com"
 		));
 
-		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
-		when(jwtService.getUserId(any())).thenReturn(10L);
-		doNothing().when(commandExpenseService).updateImgUrl(anyLong(), anyLong(), eq(expenseId), any());
-
 		mockMvc.perform(put("/api/v1/expenses/img/{expenseId}", expenseId)
 				.param("groupToken", groupToken)
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(request)))
+				.content(objectMapper.writeValueAsString(request)))
 			.andExpect(status().isOk());
 	}
 
 	@Test
-	@DisplayName("참여자를 찾을 수 없으면 예외를 반환한다 - saveExpenses")
+	@DisplayName("참여자를 찾을 수 없으면 예외를 반환한다.")
 	void saveExpensesFail_whenMemberNotFound() throws Exception {
 		ExpensesRequest request = new ExpensesRequest(
 			List.of(new ExpenseRequest(100000L, "지출", LocalDate.now(), List.of())));
@@ -144,24 +250,12 @@ public class ExpenseControllerTest extends RestDocsTestSupport {
 	}
 
 	@Test
-	@DisplayName("지출 내역을 찾을 수 없으면 예외를 반환한다 - getByExpenseId")
+	@DisplayName("지출 내역을 찾을 수 없으면 예외를 반환한다.")
 	void getByExpenseIdFail_whenExpenseNotFound() throws Exception {
 		when(queryExpenseService.findOneByExpenseId(expenseId))
 			.thenThrow(new ExpenseNotFoundException(expenseId));
 
 		mockMvc.perform(get("/api/v1/expenses/{expenseId}", expenseId))
-			.andExpect(status().isNotFound());
-	}
-
-	@Test
-	@DisplayName("지출 내역을 찾을 수 없으면 예외를 반환한다 - getAllByGroupId")
-	void getAllByGroupIdFail_whenExpenseNotFound() throws Exception {
-		when(jwtService.getGroupId(groupToken)).thenReturn(groupId);
-		when(queryExpenseService.findAllByGroupId(groupId))
-			.thenThrow(new ExpenseNotFoundException(expenseId));
-
-		mockMvc.perform(get("/api/v1/expenses")
-				.param("groupToken", groupToken))
 			.andExpect(status().isNotFound());
 	}
 }
