c3a9422ae861115bca726a797de5f368fba06d0a
원지윤 <waga1009@gmail.com>
2025-08-24 21:57:53 +0900

    fix: claims email삭제에 따른 오류 수정 - loadByUserName 수정

--- src/main/java/com/dnd/moddo/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/dnd/moddo/global/security/auth/AuthDetailsService.java
@@ -1,25 +1,27 @@
 package com.dnd.moddo.global.security.auth;
 
-import com.dnd.moddo.domain.user.exception.UserNotFoundException;
-import com.dnd.moddo.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dnd.moddo.domain.user.exception.UserNotFoundException;
+import com.dnd.moddo.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class AuthDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .map(AuthDetails::new)
-                .orElseThrow(() -> new UserNotFoundException(email));
-    }
+	@Override
+	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+		return userRepository.findById(Long.parseLong(id))
+			.map(AuthDetails::new)
+			.orElseThrow(() -> new UserNotFoundException(id));
+	}
 }
