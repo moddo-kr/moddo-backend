1587529fcda6b736bebc0075cb7e97a12709223d
원지윤 <waga1009@gmail.com>
2025-11-29 23:06:35 +0900

    feat: runtime exception 발생시 디스코드 웹 훅 추가

--- src/main/java/com/dnd/moddo/domain/settlement/controller/SettlementController.java
+++ src/main/java/com/dnd/moddo/domain/settlement/controller/SettlementController.java
@@ -31,7 +31,7 @@ public class SettlementController {
 	private final CommandSettlementService commandSettlementService;
 	private final JwtService jwtService;
 	private final QuerySettlementService querySettlementService;
-
+	
 	@PostMapping
 	public ResponseEntity<SettlementSaveResponse> saveSettlement(HttpServletRequest request,
 		@RequestBody SettlementRequest settlementRequest) {
@@ -59,7 +59,6 @@ public class SettlementController {
 		@RequestParam("groupToken") String code) {
 		Long userId = jwtService.getUserId(request);
 		Long settlementId = querySettlementService.findIdByCode(code);
-
 		SettlementDetailResponse response = querySettlementService.findOne(settlementId, userId);
 		return ResponseEntity.ok(response);
 	}
