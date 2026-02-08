2bc7d3fb8c3e13b377b53e3b6d41da7ea4898cc3
원지윤 <waga1009@gmail.com>
2026-02-08 17:35:00 +0900

    fix: discord webhook 오류 수정

--- src/main/java/com/dnd/moddo/common/logging/DiscordErrorFeignClient.java
+++ src/main/java/com/dnd/moddo/common/logging/DiscordErrorFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "${discord.error-name}", url = "${discord.error-webhook-url}")
+@FeignClient(name = "error", url = "${discord.webhook-url}")
 public interface DiscordErrorFeignClient {
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	void sendMessage(@RequestBody DiscordMessage discordMessage);
