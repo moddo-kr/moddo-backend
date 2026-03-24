c18e5ca8b7cec7c843039e9f93106e278807095c
원지윤 <waga1009@gmail.com>
2026-03-24 23:00:35 +0900

    refactor: outbox패턴 pending 스케쥴러 예외 처리 추가

--- src/main/java/com/dnd/moddo/outbox/application/impl/OutboxEventScheduler.java
+++ src/main/java/com/dnd/moddo/outbox/application/impl/OutboxEventScheduler.java
@@ -4,14 +4,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OutboxEventScheduler {
 	private final OutboxEventPublisher outboxEventPublisher;
 
 	@Scheduled(fixedDelay = 5000)
 	public void publishPendingEvents() {
-		outboxEventPublisher.publishPendingEvents();
+		try {
+			outboxEventPublisher.publishPendingEvents();
+		} catch (Exception e) {
+			log.error("Error publishing events to outbox events queue", e);
+		}
 	}
 }
